81cbbe9bae4d1f001fecdcf6374207c82ded93a0
brianlam38 <brian.lam38@gmail.com>
2018-01-25 16:08:01 +1100

    adding dynamic vars in startup script

--- startup.bash
+++ startup.bash
@@ -12,6 +12,10 @@ echo '============================================================'
 echo '               SETTING UP PIPELINE VARIABLES                '
 echo '============================================================'
 
+# grab repository / directory name
+foldername="$(basename $PWD)"
+default="_default"
+
 # remove old env variables
 > common/networks.txt
 > common/api_keys.txt
@@ -19,14 +23,14 @@ echo '... Old env variables removed successfully'
 
 # get StackStorm container IP (for use by DefectDojo)
 ss_key="SS_IP="
-ss_value=$(sudo docker network inspect banzai_default | grep -A 3 'stackstorm' | grep 'IPv4' | sed 's/.*: "//' | sed 's/\/.*//')
+ss_value=$(sudo docker network inspect $foldername$default | grep -A 3 'stackstorm' | grep 'IPv4' | sed 's/.*: "//' | sed 's/\/.*//')
 ss_ip=$ss_key$ss_value
 echo $ss_ip >> common/networks.txt
 echo '... StackStorm container IP retrieved'
 
 # get DefectDojo container IP (for use by StackStorm)
 dd_key="DD_IP="
-dd_value=$(sudo docker network inspect banzai_default | grep -A 3 'banzai_dojo_1' | grep 'IPv4' | sed 's/.*: "//' | sed 's/\/.*//')
+dd_value=$(sudo docker network inspect $foldername$default | grep -A 3 'dojo' | grep 'IPv4' | sed 's/.*: "//' | sed 's/\/.*//')
 dd_ip=$dd_key$dd_value
 echo $dd_ip >> common/networks.txt
 echo '... DefectDojo container IP retrieved'
@@ -34,7 +38,7 @@ echo '... DefectDojo container IP retrieved'
 
 # get Burp container IP (for use by StackStorm)
 burp_key="BURP_IP="
-burp_value=$(sudo docker network inspect banzai_default | grep -A 3 'burpdock' | grep 'IPv4' | sed 's/.*: "//' | sed 's/\/.*//')
+burp_value=$(sudo docker network inspect $foldername$default | grep -A 3 'burpdock' | grep 'IPv4' | sed 's/.*: "//' | sed 's/\/.*//')
 burp_ip=$burp_key$burp_value
 echo $burp_ip >> common/networks.txt
 echo '... Burp container IP retrieved'
@@ -62,7 +66,7 @@ echo '                       ADJUSTING SETTINGS                   '
 echo '============================================================'
 
 # adjusting DefectDojo "ALLOWED_HOSTS" settings
-sudo docker exec banzai_dojo_1 sed -i 's/ALLOWED_HOSTS = \[\]/ALLOWED_HOSTS = \[\"\*\"\]/' /opt/django-DefectDojo/dojo/settings.py
+sudo docker exec dojo sed -i 's/ALLOWED_HOSTS = \[\]/ALLOWED_HOSTS = \[\"\*\"\]/' /opt/django-DefectDojo/dojo/settings.py
 echo '... Django settings adjusted'
 
 echo '============================================================'
